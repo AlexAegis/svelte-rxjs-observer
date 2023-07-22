d47752b1a886877534c01968ad4f0c3ec3593112
AlexAegis <alexaegis@gmail.com>
2023-07-22 17:47:21 +0200

    feat: example app now uses skeletonui and has code blocks

--- apps/example/src/routes/+layout.svelte
+++ apps/example/src/routes/+layout.svelte
@@ -1,1 +1,46 @@
-<slot />
+<script>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import { AppBar, AppShell, storeHighlightJs } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	import packageJson from '../../../../package.json';
+	import '../app.postcss';
+	storeHighlightJs.set(hljs);
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">{packageJson.name}</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="{packageJson.homepage}"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<div class="main-layout">
+		<slot />
+	</div>
+</AppShell>
+
+<style>
+	.main-layout {
+		padding: 1em;
+		max-width: 800px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
--- apps/example/src/routes/+page.svelte
+++ apps/example/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { CodeBlock } from '@skeletonlabs/skeleton';
 	import { Subject } from 'rxjs';
 	import packageJson from '../../../../package.json';
 
@@ -16,12 +17,35 @@
 <div class="app">
 	<h1>{packageJson.name} ({packageJson.version})</h1>
 	<div class="controls">
-		<button on:click="{reset}"> Reset </button>
-		<button on:click="{() => subject.next((count = count + 1))}"> Next </button>
-		<button on:click="{() => subject.error('Error message')}"> Error </button>
-		<button on:click="{() => subject.complete()}"> Complete </button>
+		<button type="button" class="btn variant-filled" on:click="{reset}"> Reset </button>
+		<button
+			type="button"
+			class="btn variant-filled"
+			on:click="{() => subject.next((count = count + 1))}"
+		>
+			Next
+		</button>
+		<button
+			type="button"
+			class="btn variant-filled"
+			on:click="{() => subject.error('Error message')}"
+		>
+			Error
+		</button>
+		<button type="button" class="btn variant-filled" on:click="{() => subject.complete()}">
+			Complete
+		</button>
 	</div>
-	{$subject}
+
+	<h2>Value with regular $subject:</h2>
+	<p>
+		{$subject}
+	</p>
+
+	<CodeBlock language="html" code="{`{$subject}`}" />
+
+	<h2>Value with the Observer component</h2>
+
 	<div>
 		<Observer observable="{subject}" let:next>
 			Next: {next}
@@ -35,7 +59,23 @@
 		</Observer>
 	</div>
 
-	<span> (After an error or a complete emission an observable won't emit anymore) </span>
+	<CodeBlock
+		language="html"
+		code="{`
+<Observer observable="{subject}" let:next>
+	Next: {next}
+	<svelte:fragment slot="pending">Pending</svelte:fragment>
+	<svelte:fragment slot="error" let:error let:last>
+		Error: {error}. last received: {last}
+	</svelte:fragment>
+	<svelte:fragment slot="completed" let:last>
+		Completed. Last received: {last}
+	</svelte:fragment>
+</Observer>
+		`}"
+	/>
+
+	<span> (After an 'error' or a 'complete' emission, an observable won't emit anymore) </span>
 </div>
 
 <style>
